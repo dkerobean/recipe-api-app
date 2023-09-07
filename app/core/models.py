4c7ee1bf4856fb0dd67041da5eb12fb1693bd1dd
Dickson <dkerobean@gmail.com>
2023-09-07 17:42:43 +0000

    user model

--- app/core/models.py
+++ app/core/models.py
@@ -8,7 +8,9 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('email must not be empty')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
