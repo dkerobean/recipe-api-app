b6d75ab514506ab71f49afb79c8e77ed3bbf7cd6
Dickson <dkerobean@gmail.com>
2023-09-07 12:14:20 +0000

    database test done

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+"""
+Django commands to wait for the database to be available
+"""
+import time
+from django.core.management.base import BaseCommand
+
+from psycopg2 import OperationalError as Psycopg2OpError
+from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand  # noqa
+
+
+class Command(BaseCommand):
+    """ Django commands to wait for the database to be available """
+
+    def handle(self, *args, **kwargs):
+        """ Entry point for command """
+        self.stdout.write("Waiting for database to be available...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write("Database is not available, waiting...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS('Database is available'))
